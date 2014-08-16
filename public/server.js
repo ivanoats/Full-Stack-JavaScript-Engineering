0b84bec438866a93f12f53a80eb82556b66bfbe7
Ivan Storck <ivanoats@gmail.com>
2014-08-16 14:18:04 -0700

    adding backbone pages to public

--- public/server.js
+++ public/server.js
@@ -1,5 +1,6 @@
-var express = require('express'); 
+var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
+console.log('gitbook express server started');
